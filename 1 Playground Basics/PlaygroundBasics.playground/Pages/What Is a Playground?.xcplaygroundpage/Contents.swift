302c74ef2d8d5b7bf70b022d942827377e4f2138
Siddharth Muralikrishnan <693981@pdsb.net>
2020-03-11 12:34:42 -0400

    Lesson 1. I'll send the rest if you're able to view this

---
+++ 1 Playground Basics/PlaygroundBasics.playground/Pages/What Is a Playground?.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+660
+//:👆Notice how the number above looks different?
+//: This style of text is *editable code*, which means you can write it or change it to make stuff happen.
+//:
+//: For negative numbers use the minus sign:
+600
+/*:
+Notice how the numbers also show up in the gray area to the right? 👉
+
+That area is called the _results sidebar_. As you add or change code, the playground runs your code again and updates the results in the sidebar.
+ 
+- experiment:
+    - Click a line of code to move the cursor there.
+    - Type to start editing code.
+    - Change the numbers a few times.
+    - Add a few new numbers, each on a separate line.\
+Notice that every time you make a change the results are updated in the sidebar.
+
+Next, find out how to put the results sidebar to better use.
+ 
+ [Previous](@previous)  |  page 2 of 7  |  [Next: Calculations](@next)
+*/
